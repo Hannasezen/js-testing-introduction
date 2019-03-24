dbeb25dd44cda0d408b71991074acd69da063a3c
Hanna Sezen <andreeva0649@gmail.com>
2019-03-24 12:54:51 +0200

    unit test 2

--- util.test.js
+++ util.test.js
@@ -3,4 +3,8 @@ const { generateText } = require('./util');
 test('should output name and age', () => {
   const text = generateText('Max', 29);
   expect(text).toBe('Max (29 years old)');
+  /*const text1 = generateText('', null);
+  expect(text1).toBe(' (null years old)');
+  const text2 = generateText();
+  expect(text2).toBe('undefined (undefined years old)');*/
 });
